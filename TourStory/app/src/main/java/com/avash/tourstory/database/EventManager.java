3d3f93236ece536e6faac080bc0bfe47fd2d8b90
Shariful Haque Avash <avash10@yahoo.com>
2017-02-03 22:19:25 +0600

    Event Added

---
+++ TourStory/app/src/main/java/com/avash/tourstory/database/EventManager.java
@@ -0,0 +1,8 @@
+package com.avash.tourstory.database;
+
+/**
+ * Created by avash on 2/3/2017.
+ */
+
+public class EventManager {
+}
---
+++ TourStory/app/src/main/java/com/avash/tourstory/model/EventModel.java
@@ -0,0 +1,8 @@
+package com.avash.tourstory.model;
+
+/**
+ * Created by avash on 2/3/2017.
+ */
+
+public class EventModel {
+}
